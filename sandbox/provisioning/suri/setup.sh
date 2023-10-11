eed7e76c2388111fc8644c7a36ed31a27490c21e
xslizik <99902995+xslizik@users.noreply.github.com>
2023-10-11 15:02:07 +0200

    Update setup.sh

--- sandbox/provisioning/suri/setup.sh
+++ sandbox/provisioning/suri/setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-sudo su
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 source "$HOME/.cargo/env"
 rustup default stable
 rustup update
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' | sudo tee -a /etc/profile
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' > -a /etc/profile
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cd /home/vagrant/suricata-7.0.1
 ./configure && make && make install
 ldconfig
-suricata-update update-sources
-suricata-update enable-source et/open
